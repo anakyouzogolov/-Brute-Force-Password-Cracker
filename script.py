e40f4c9883b4715595610d7942426efc9f4da112
anakyouzogolov <64495296+anakyouzogolov@users.noreply.github.com>
2020-06-08 05:14:08 -0700

    Add files via upload

---
+++ script.py
@@ -0,0 +1,23 @@
+import random
+import pyautogui
+import string
+
+
+chars = "abcdefghijklmnopqrstuvwxyz0123456789"
+
+# chars = string.printable
+chars_list = list(chars)
+
+
+password = pyautogui.password("Enter a password : ")
+
+guess_password = ""
+
+while(guess_password != password):
+    guess_password = random.choices(chars_list, k=len(password))
+
+    print("<=================="+ str(guess_password)+ "==================>")
+
+    if(guess_password == list(password)):
+        print("Your password is : "+ "".join(guess_password))
+        break
